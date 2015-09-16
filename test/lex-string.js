a2e2accb91d4ca6ea2497b7bfae6335b12c194d4
Zhenzhen Zhan <zhanzhenzhen@hotmail.com>
2015-09-16 21:44:37 +0800

    test

--- test/lex-string.js
+++ test/lex-string.js
@@ -8,7 +8,7 @@ x: "abc"
 x: "
     abc
 "
-`).toString()
+`).toString();
 console.log(s === 'NormalToken "x", Colon, InlineNormalString, CallLeftParenthesis, Str "abc", RightParenthesis, Semicolon, NormalToken "x", Colon, FormattedNormalString, CallLeftParenthesis, Str "abc", RightParenthesis');
 
 // Each last line of the first 5 strings should be treated as empty. But the last line of the last string should be treated as not empty. These 6 strings only have differences (of the number of spaces) in their last lines.
@@ -49,5 +49,13 @@ s = new $lex.Lex(`lemo 0.1.0
             def
             
     "
-`).toString()
+`).toString();
 console.log(s === 'DashFunction, LeftChevron, FormattedNormalString, CallLeftParenthesis, Str "abc\\\\n    def\\\\n", RightParenthesis, Semicolon, FormattedNormalString, CallLeftParenthesis, Str "abc\\\\n    def\\\\n", RightParenthesis, Semicolon, FormattedNormalString, CallLeftParenthesis, Str "abc\\\\n    def\\\\n", RightParenthesis, Semicolon, FormattedNormalString, CallLeftParenthesis, Str "abc\\\\n    def\\\\n", RightParenthesis, Semicolon, FormattedNormalString, CallLeftParenthesis, Str "abc\\\\n    def\\\\n", RightParenthesis, Semicolon, FormattedNormalString, CallLeftParenthesis, Str "abc\\\\n    def\\\\n    ", RightParenthesis, RightChevron');
+
+s = new $lex.Lex(`lemo 0.1.0
+x: v"C:\\Windows"
+x: v"
+    C:\\Windows
+"
+`).toString();
+console.log(s === 'NormalToken "x", Colon, InlineVerbatimString, CallLeftParenthesis, Str "C:\\\\Windows", RightParenthesis, Semicolon, NormalToken "x", Colon, FormattedVerbatimString, CallLeftParenthesis, Str "C:\\\\Windows", RightParenthesis');
