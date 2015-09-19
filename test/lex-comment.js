0bf485a6110bb66e09f1c56e702fbcdd7be0381f
Zhenzhen Zhan <zhanzhenzhen@hotmail.com>
2015-09-20 05:14:26 +0800

    lex

---
+++ test/lex-comment.js
@@ -0,0 +1,21 @@
+import * as $lex from "../lib/compile-lex-0";
+import assert from "assert";
+
+let s = null;
+
+s = new $lex.Lex(`lemo 0.1.0
+
+
+
+###
+Long comment!
+###
+
+# Short comment!
+abc() # abc
+
+###
+Footer!
+###
+`).toString();
+console.log(s === 'FormattedComment "\\nLong comment!\\n", Semicolon, NormalToken "abc", CallLeftParenthesis, RightParenthesis');
