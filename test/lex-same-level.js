bbd77f9aa127532259d3b29f3c265f57118056cd
Zhenzhen Zhan <zhanzhenzhen@hotmail.com>
2015-09-25 15:32:47 +0800

    misc

---
+++ test/lex-same-level.js
@@ -0,0 +1,27 @@
+import * as $lex from "../lib/compile-lex-0";
+import assert from "assert";
+
+let s = null;
+
+s = new $lex.Lex(`lemo 0.1.0, node module
+aaa(); bbb()
+ccc()
+`).toString();
+console.log(s === 'NormalToken "aaa", CallLeftParenthesis, RightParenthesis, Semicolon, NormalToken "bbb", CallLeftParenthesis, RightParenthesis, Semicolon, NormalToken "ccc", CallLeftParenthesis, RightParenthesis');
+
+s = new $lex.Lex(`lemo 0.1.0, node module
+;;
+;aaa();;; bbb();;
+;   ccc(); ;
+;
+`).toString();
+console.log(s === 'NormalToken "aaa", CallLeftParenthesis, RightParenthesis, Semicolon, NormalToken "bbb", CallLeftParenthesis, RightParenthesis, Semicolon, NormalToken "ccc", CallLeftParenthesis, RightParenthesis');
+
+s = new $lex.Lex(`lemo 0.1.0, node module
+<>
+    ;
+    aaa()
+    ;
+bbb()
+`).toString();
+console.log(s === 'DiamondFunction, LeftChevron, NormalToken "aaa", CallLeftParenthesis, RightParenthesis, RightChevron, Semicolon, NormalToken "bbb", CallLeftParenthesis, RightParenthesis');
