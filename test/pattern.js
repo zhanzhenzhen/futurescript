e5af154c3609f887351073445d07d11491269c0f
Zhenzhen Zhan <zhanzhenzhen@hotmail.com>
2015-10-15 05:03:48 +0800

    test

--- test/pattern.js
+++ test/pattern.js
@@ -13,7 +13,7 @@ result = $pattern.Pattern.searchSequence(
     {lex: lex, startIndex: 0, endIndex: lex.value.length - 1},
     true
 );
-console.log(result);
+console.log(result === 0);
 
 lex = new $lex.Lex(`lemo 0.1.0, node module
 if a = 1
@@ -21,22 +21,66 @@ if a = 1
 else
     b: 2
 `);
-console.log(lex);
+
 result = $pattern.Pattern.searchPattern(
     [$pattern.Any, $lex.NormalToken, $lex.Equal, $pattern.Any],
     {lex: lex, startIndex: 0, endIndex: lex.value.length - 1},
     true
 );
-console.log(result);
+console.log(result.length === 4);
+console.log(result[0] === 0);
+console.log(result[1] === 1);
+console.log(result[2] === 2);
+console.log(result[3] === 3);
+
 result = $pattern.Pattern.searchPattern(
     [$pattern.Tokens, $lex.NormalToken, $lex.Equal, $pattern.Tokens],
     {lex: lex, startIndex: 0, endIndex: lex.value.length - 1},
     true
 );
-console.log(result);
+console.log(result.length === 4);
+console.log(result[0] === 0);
+console.log(result[1] === 1);
+console.log(result[2] === 2);
+console.log(result[3] === 3);
+
 result = $pattern.Pattern.searchPattern(
     [$lex.If, $pattern.Any, $pattern.ChevronPair, $lex.Else, $pattern.ChevronPair],
     {lex: lex, startIndex: 0, endIndex: lex.value.length - 1},
     true
+);
+console.log(result.length === 5);
+console.log(result[0] === 0);
+console.log(result[1] === 1);
+console.log(result[2] === 4);
+console.log(result[3] === 9);
+console.log(result[4] === 10);
+
+result = $pattern.Pattern.searchPattern(
+    [$lex.If, $pattern.Tokens, $pattern.ChevronPair, $lex.Else, $pattern.ChevronPair],
+    {lex: lex, startIndex: 0, endIndex: lex.value.length - 1},
+    true
+);
+console.log(result.length === 5);
+console.log(result[0] === 0);
+console.log(result[1] === 1);
+console.log(result[2] === 4);
+console.log(result[3] === 9);
+console.log(result[4] === 10);
+
+result = $pattern.Pattern.searchPattern(
+    [$pattern.Tokens, $lex.Else, $pattern.ChevronPair],
+    {lex: lex, startIndex: 0, endIndex: lex.value.length - 1},
+    true
+);
+console.log(result);
+
+lex = new $lex.Lex(`lemo 0.1.0, node module
+a ? b | c + d ? e | f
+`);
+result = $pattern.Pattern.searchPattern(
+    [$pattern.Tokens, $lex.Then, $pattern.Tokens, $lex.Else, $pattern.Tokens],
+    {lex: lex, startIndex: 0, endIndex: lex.value.length - 1},
+    false
 );
 console.log(result);
