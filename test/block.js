a0071f7e019b5dd1098951e2a80c1c1c8440702d
Zhenzhen Zhan <zhanzhenzhen@hotmail.com>
2015-10-22 06:39:57 +0800

    test

---
+++ test/block.js
@@ -0,0 +1,12 @@
+import * as $lex from "../lib/compile-lex-0";
+import * as $block from "../lib/compile-block-0";
+import assert from "assert";
+
+let s = null;
+let lex, result;
+
+lex = new $lex.Lex(`lemo 0.1.0, node module
+a: 1 + 2
+`);
+let block = new $block.Block({lex: lex, startIndex: 0, endIndex: lex.count() - 1});
+console.log(block);
