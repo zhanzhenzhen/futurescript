297261a38d5228644761496863f7b856a7cf9c07
Zhenzhen Zhan <zhanzhenzhen@hotmail.com>
2015-10-23 04:58:08 +0800

    fix

--- test/block.js
+++ test/block.js
@@ -9,8 +9,13 @@ lex = new $lex.Lex(`lemo 0.1.0, node module
 a: 1 + 2 + 3
 b: 2 + 3
 c: abc(5, 6)
+if a
+    c: 7
+else
+    c: 5
 `);
 let block = new $block.Block({lex: lex, startIndex: 0, endIndex: lex.count() - 1});
 console.log(block);
 console.log(block.print());
 console.log(block.value[2]);
+console.log(block.value[3]);
