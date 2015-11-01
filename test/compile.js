095414e7ebe3c365122d955568caf9e88072603d
Zhenzhen Zhan <zhanzhenzhen@hotmail.com>
2015-11-02 07:25:42 +0800

    compile

--- test/compile.js
+++ test/compile.js
@@ -3,7 +3,11 @@ import compile from "../lib/compile";
 let output;
 
 output = compile({code: `lemo 0.1.0, node module
-
 a: 123
+`, path: "abc.lemo"});
+console.log(output);
+
+output = compile({code: `lemo 0.1.0, node module
+a: 123 + 456 and 3
 `, path: "abc.lemo"});
 console.log(output);
