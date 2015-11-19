71d65e38dbf952b7bcc448e36e9af9ac296f1408
Zhenzhen Zhan <zhanzhenzhen@hotmail.com>
2015-11-19 15:33:04 +0800

    test directory structure

---
+++ test/c-tree-match-0.js
@@ -0,0 +1,47 @@
+import * as $lex from "../lib/c-lex-0.js";
+import * as $block from "../lib/c-block-0.js";
+
+let lex, block;
+
+lex = new $lex.Lex(`lemo 0.1.0, node module
+match a
+    1 ? 10
+    2 ? 100
+    |   0
+`);
+block = new $block.RootBlock(lex);
+console.log(block.toString() === `node module
+RootBlock [
+    ExpressionStatement {
+        expression: MatchExpression {
+            comparer: VariableExpression "a"
+            items: Arr [
+                Xy {
+                    x: NumberExpression "1"
+                    y: Block [
+                        ExpressionStatement {
+                            expression: NumberExpression "10"
+                        }
+                    ]
+                }
+                Xy {
+                    x: NumberExpression "2"
+                    y: Block [
+                        ExpressionStatement {
+                            expression: NumberExpression "100"
+                        }
+                    ]
+                }
+                Xy {
+                    x: null
+                    y: Block [
+                        ExpressionStatement {
+                            expression: NumberExpression "0"
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+]
+`);
--- test/c-v0.1.0.js
+++ test/c-v0.1.0.js
@@ -3,6 +3,7 @@ import "../lib/c-shim-0.js";
 import "./c-tools-0.js";
 import "./c-tree-0.js";
 import "./c-tree-if-0.js";
+import "./c-tree-match-0.js";
 import "./c-lex-0.js";
 import "./c-lex-comment-0.js";
 import "./c-lex-connect-0.js";
