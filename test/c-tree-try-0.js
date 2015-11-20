3008d0c95290d2890fd7573b02ffb3f6ad6eb39b
Zhenzhen Zhan <zhanzhenzhen@hotmail.com>
2015-11-20 12:17:34 +0800

    misc

--- test/c-tree-try-0.js
+++ test/c-tree-try-0.js
@@ -81,3 +81,49 @@ RootBlock [
     }
 ]
 `);
+
+lex = new $lex.Lex(`lemo 0.1.0, node module
+a:
+    try
+        b
+    catch undefined
+        throw undefined + 1
+`);
+block = new $block.RootBlock(lex);
+block.complyWithJs();
+console.log(block.toString() === `node module
+RootBlock [
+    AssignStatement {
+        assignees: Arr [
+            VariableAssignee {
+                export: false
+                ifnull: false
+                ifvoid: false
+                variable: Piece "a"
+            }
+        ]
+        value: TryExpression {
+            catchBranch: Block [
+                ThrowStatement {
+                    value: PlusExpression {
+                        x: VariableExpression "var_573300145710716007_0"
+                        y: NumberExpression "1"
+                    }
+                }
+            ]
+            catchVar: VariableAssignee {
+                export: false
+                ifnull: false
+                ifvoid: false
+                variable: Piece "var_573300145710716007_0"
+            }
+            finallyBranch: null
+            tryBranch: Block [
+                ExpressionStatement {
+                    expression: VariableExpression "b"
+                }
+            ]
+        }
+    }
+]
+`);
