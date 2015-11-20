c2771e1d14288e3de2f7880dc774b040dd6f2e5e
Zhenzhen Zhan <zhanzhenzhen@hotmail.com>
2015-11-20 20:54:45 +0800

    fix

--- test/c-tree-match-0.js
+++ test/c-tree-match-0.js
@@ -45,3 +45,49 @@ RootBlock [
     }
 ]
 `);
+
+lex = new $lex.Lex(`lemo 0.1.0, node module
+match a
+    1
+        10
+    2
+        100
+    |
+        0
+`);
+block = new $block.RootBlock(lex);
+console.log(block.toString() === `node module
+RootBlock [
+    ExpressionStatement {
+        expression: MatchExpression {
+            comparer: VariableExpression "a"
+            items: Arr [
+                Xy {
+                    x: NumberExpression "1"
+                    y: Block [
+                        ExpressionStatement {
+                            expression: NumberExpression "10"
+                        }
+                    ]
+                }
+                Xy {
+                    x: NumberExpression "2"
+                    y: Block [
+                        ExpressionStatement {
+                            expression: NumberExpression "100"
+                        }
+                    ]
+                }
+                Xy {
+                    x: null
+                    y: Block [
+                        ExpressionStatement {
+                            expression: NumberExpression "0"
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+]
+`);
