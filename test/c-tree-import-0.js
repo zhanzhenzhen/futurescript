d148fcebe66560f824f9e9808e2cc8f1d4066672
Zhenzhen Zhan <zhanzhenzhen@hotmail.com>
2015-11-26 01:01:54 +0800

    test

--- test/c-tree-import-0.js
+++ test/c-tree-import-0.js
@@ -6,29 +6,20 @@ let lex, block;
 lex = new $lex.Lex(`lemo 0.1.0, node module
 import "aaa"
 `);
-console.log(lex.toString());
 block = new $block.RootBlock(lex);
-console.log(block.toString());
 console.log(block.toString() === `node module
 RootBlock [
     ExpressionStatement {
-        expression: TryExpression {
-            catchBranch: Block [
-                ExpressionStatement {
-                    expression: VariableExpression "b"
-                }
-            ]
-            catchVar: null
-            finallyBranch: Block [
-                ExpressionStatement {
-                    expression: VariableExpression "c"
-                }
-            ]
-            tryBranch: Block [
-                ExpressionStatement {
-                    expression: VariableExpression "a"
-                }
-            ]
+        expression: ImportExpression {
+            batchall: false
+            catchall: null
+            mapping: null
+            module: TinyCallExpression {
+                arguments: Arr [
+                    StringExpression "aaa"
+                ]
+                callee: InlineNormalStringExpression
+            }
         }
     }
 ]
