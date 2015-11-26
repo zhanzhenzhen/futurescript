70662002b7bcc15cf9e52f5fa65063113e73b346
Zhenzhen Zhan <zhanzhenzhen@hotmail.com>
2015-11-26 14:07:22 +0800

    import

--- test/c-tree-import-0.js
+++ test/c-tree-import-0.js
@@ -29,14 +29,53 @@ lex = new $lex.Lex(`lemo 0.1.0, node module
 aaa: import "aaa"
 `);
 block = new $block.RootBlock(lex);
-console.log(block.toString());
+console.log(block.toString() === `node module
+RootBlock [
+    ImportStatement {
+        batchall: false
+        catchall: null
+        mapping: Arr [
+            Xy {
+                x: VariableAssignee {
+                    export: false
+                    ifnull: false
+                    ifvoid: false
+                    variable: Piece "aaa"
+                }
+                y: Piece "default"
+            }
+        ]
+        module: TinyCallExpression {
+            arguments: Arr [
+                StringExpression "aaa"
+            ]
+            callee: InlineNormalStringExpression
+        }
+    }
+]
+`);
+
+lex = new $lex.Lex(`lemo 0.1.0, node module
+import "aaa" as aaa
+`);
+block = new $block.RootBlock(lex);
 console.log(block.toString() === `node module
 RootBlock [
     ExpressionStatement {
         expression: ImportExpression {
             batchall: false
             catchall: null
-            mapping: null
+            mapping: Arr [
+                Xy {
+                    x: VariableAssignee {
+                        export: false
+                        ifnull: false
+                        ifvoid: false
+                        variable: Piece "aaa"
+                    }
+                    y: Piece "default"
+                }
+            ]
             module: TinyCallExpression {
                 arguments: Arr [
                     StringExpression "aaa"
