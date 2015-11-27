471e551474a588f01a9d37bc1cd587bfdefc457b
Zhenzhen Zhan <zhanzhenzhen@hotmail.com>
2015-11-27 09:50:56 +0800

    import

--- test/c-tree-import-0.js
+++ test/c-tree-import-0.js
@@ -216,3 +216,85 @@ RootBlock [
     }
 ]
 `);
+
+lex = new $lex.Lex(`lemo 0.1.0, node module
+import "aaa" as {a, b as c}
+`);
+block = new $block.RootBlock(lex);
+console.log(block.toString() === `node module
+RootBlock [
+    ExpressionStatement {
+        expression: ImportExpression {
+            batchall: false
+            catchall: null
+            mapping: Arr [
+                Xy {
+                    x: VariableAssignee {
+                        export: false
+                        ifnull: false
+                        ifvoid: false
+                        variable: Piece "a"
+                    }
+                    y: Piece "a"
+                }
+                Xy {
+                    x: VariableAssignee {
+                        export: false
+                        ifnull: false
+                        ifvoid: false
+                        variable: Piece "c"
+                    }
+                    y: Piece "b"
+                }
+            ]
+            module: PseudoCallExpression {
+                arguments: Arr [
+                    StringExpression "aaa"
+                ]
+                callee: InlineNormalStringExpression
+            }
+        }
+    }
+]
+`);
+
+lex = new $lex.Lex(`lemo 0.1.0, node module
+import "aaa" all as {a, b as c}
+`);
+block = new $block.RootBlock(lex);
+console.log(block.toString() === `node module
+RootBlock [
+    ExpressionStatement {
+        expression: ImportExpression {
+            batchall: false
+            catchall: null
+            mapping: Arr [
+                Xy {
+                    x: VariableAssignee {
+                        export: false
+                        ifnull: false
+                        ifvoid: false
+                        variable: Piece "a"
+                    }
+                    y: Piece "a"
+                }
+                Xy {
+                    x: VariableAssignee {
+                        export: false
+                        ifnull: false
+                        ifvoid: false
+                        variable: Piece "c"
+                    }
+                    y: Piece "b"
+                }
+            ]
+            module: PseudoCallExpression {
+                arguments: Arr [
+                    StringExpression "aaa"
+                ]
+                callee: InlineNormalStringExpression
+            }
+        }
+    }
+]
+`);
