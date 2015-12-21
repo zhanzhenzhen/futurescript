23d44d8128dae5d5f097653087d1b314b457b471
Zhenzhen Zhan <zhanzhenzhen@hotmail.com>
2015-12-21 14:30:05 +0800

    misc

--- test/c-tree-function-0.js
+++ test/c-tree-function-0.js
@@ -162,3 +162,96 @@ RootBlock [
 ]
 `);
 }); // ============================================================
+
+test(() => {
+lex = new $lex.Lex(`fus 0.1.0, node module
+a: [x, y] -> x + y
+`);
+block = new $node.RootBlock(lex);
+console.log(block.toString());
+assert(block.toString() === `node module
+RootBlock [
+    AssignStatement {
+        assignees: Arr [
+            VariableAssignee {
+                export: false
+                ifnull: false
+                ifvoid: false
+                variable: LocalVariable "a"
+            }
+        ]
+        value: ArrowFunctionExpression {
+            arguments: Arr [
+                ArrowArgument {
+                    nullDefault: null
+                    variable: LocalVariable "x"
+                    voidDefault: null
+                }
+            ]
+            body: ScopeBlock [
+                ExpressionStatement {
+                    expression: PlusExpression {
+                        x: VariableExpression "x"
+                        y: NumberExpression "1"
+                    }
+                }
+            ]
+        }
+    }
+    AssignStatement {
+        assignees: Arr [
+            VariableAssignee {
+                export: false
+                ifnull: false
+                ifvoid: false
+                variable: LocalVariable "a"
+            }
+        ]
+        value: ArrowFunctionExpression {
+            arguments: Arr [
+                ArrowArgument {
+                    nullDefault: null
+                    variable: LocalVariable "x"
+                    voidDefault: NumberExpression "3"
+                }
+            ]
+            body: ScopeBlock [
+                ExpressionStatement {
+                    expression: PlusExpression {
+                        x: VariableExpression "x"
+                        y: NumberExpression "1"
+                    }
+                }
+            ]
+        }
+    }
+    AssignStatement {
+        assignees: Arr [
+            VariableAssignee {
+                export: false
+                ifnull: false
+                ifvoid: false
+                variable: LocalVariable "a"
+            }
+        ]
+        value: ArrowFunctionExpression {
+            arguments: Arr [
+                ArrowArgument {
+                    nullDefault: NumberExpression "3"
+                    variable: LocalVariable "x"
+                    voidDefault: null
+                }
+            ]
+            body: ScopeBlock [
+                ExpressionStatement {
+                    expression: PlusExpression {
+                        x: VariableExpression "x"
+                        y: NumberExpression "1"
+                    }
+                }
+            ]
+        }
+    }
+]
+`);
+}); // ============================================================
