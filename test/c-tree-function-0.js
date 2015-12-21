463226489b2417dd0a0cf58ac129eefed224690e
Zhenzhen Zhan <zhanzhenzhen@hotmail.com>
2015-12-21 16:56:33 +0800

    function

--- test/c-tree-function-0.js
+++ test/c-tree-function-0.js
@@ -95,6 +95,7 @@ RootBlock [
                     voidDefault: null
                 }
             ]
+            argumentsReal: true
             body: ScopeBlock [
                 ExpressionStatement {
                     expression: PlusExpression {
@@ -122,6 +123,7 @@ RootBlock [
                     voidDefault: NumberExpression "3"
                 }
             ]
+            argumentsReal: true
             body: ScopeBlock [
                 ExpressionStatement {
                     expression: PlusExpression {
@@ -149,6 +151,7 @@ RootBlock [
                     voidDefault: null
                 }
             ]
+            argumentsReal: true
             body: ScopeBlock [
                 ExpressionStatement {
                     expression: PlusExpression {
@@ -166,9 +169,9 @@ RootBlock [
 test(() => {
 lex = new $lex.Lex(`fus 0.1.0, node module
 a: [x, y] -> x + y
+b: [w, x: 3, y ifnull: 4, z ifvoid: 1] -> w + x + y + z
 `);
 block = new $node.RootBlock(lex);
-console.log(block.toString());
 assert(block.toString() === `node module
 RootBlock [
     AssignStatement {
@@ -187,12 +190,18 @@ RootBlock [
                     variable: LocalVariable "x"
                     voidDefault: null
                 }
+                ArrowArgument {
+                    nullDefault: null
+                    variable: LocalVariable "y"
+                    voidDefault: null
+                }
             ]
+            argumentsReal: false
             body: ScopeBlock [
                 ExpressionStatement {
                     expression: PlusExpression {
                         x: VariableExpression "x"
-                        y: NumberExpression "1"
+                        y: VariableExpression "y"
                     }
                 }
             ]
@@ -204,49 +213,44 @@ RootBlock [
                 export: false
                 ifnull: false
                 ifvoid: false
-                variable: LocalVariable "a"
+                variable: LocalVariable "b"
             }
         ]
         value: ArrowFunctionExpression {
             arguments: Arr [
                 ArrowArgument {
                     nullDefault: null
+                    variable: LocalVariable "w"
+                    voidDefault: null
+                }
+                ArrowArgument {
+                    nullDefault: null
                     variable: LocalVariable "x"
                     voidDefault: NumberExpression "3"
                 }
-            ]
-            body: ScopeBlock [
-                ExpressionStatement {
-                    expression: PlusExpression {
-                        x: VariableExpression "x"
-                        y: NumberExpression "1"
-                    }
-                }
-            ]
-        }
-    }
-    AssignStatement {
-        assignees: Arr [
-            VariableAssignee {
-                export: false
-                ifnull: false
-                ifvoid: false
-                variable: LocalVariable "a"
-            }
-        ]
-        value: ArrowFunctionExpression {
-            arguments: Arr [
                 ArrowArgument {
-                    nullDefault: NumberExpression "3"
-                    variable: LocalVariable "x"
+                    nullDefault: NumberExpression "4"
+                    variable: LocalVariable "y"
                     voidDefault: null
                 }
+                ArrowArgument {
+                    nullDefault: null
+                    variable: LocalVariable "z"
+                    voidDefault: NumberExpression "1"
+                }
             ]
+            argumentsReal: false
             body: ScopeBlock [
                 ExpressionStatement {
                     expression: PlusExpression {
-                        x: VariableExpression "x"
-                        y: NumberExpression "1"
+                        x: PlusExpression {
+                            x: PlusExpression {
+                                x: VariableExpression "w"
+                                y: VariableExpression "x"
+                            }
+                            y: VariableExpression "y"
+                        }
+                        y: VariableExpression "z"
                     }
                 }
             ]
