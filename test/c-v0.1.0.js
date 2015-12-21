77a62861e5d303df888c5e953aff2027e04de0ff
Zhenzhen Zhan <zhanzhenzhen@hotmail.com>
2015-12-21 11:50:45 +0800

    arrange

--- test/c-v0.1.0.js
+++ test/c-v0.1.0.js
@@ -1,4 +1,12 @@
 import "./c-tools-0.js";
+import "./c-pattern-0.js";
+
+import "./c-lex-0.js";
+import "./c-lex-comment-0.js";
+import "./c-lex-connect-0.js";
+import "./c-lex-same-level-0.js";
+import "./c-lex-string-0.js";
+
 import "./c-tree-0.js";
 import "./c-tree-assign-0.js";
 import "./c-tree-if-0.js";
@@ -8,10 +16,6 @@ import "./c-tree-try-0.js";
 import "./c-tree-import-0.js";
 import "./c-tree-export-0.js";
 import "./c-tree-class-0.js";
-import "./c-lex-0.js";
-import "./c-lex-comment-0.js";
-import "./c-lex-connect-0.js";
-import "./c-lex-same-level-0.js";
-import "./c-lex-string-0.js";
-import "./c-pattern-0.js";
+
 import "./c-compile-0.js";
+import "./c-compile-class-0.js";
